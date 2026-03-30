dbb8fc2b3e4876bace8b343b669105f4ee78137e
Rishi <rishibabusoftwaredeveloper@gmail.com>
2026-03-30 11:52:12 +0530

    Folder structure is created for Portfolio Alert application api

---
+++ PortfolioAlert/src/main/java/com/example/PortfolioAlert/PortfolioAlertApplication.java
@@ -0,0 +1,14 @@
+package com.example.PortfolioAlert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.Data;
+
+@SpringBootApplication
+public class PortfolioAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioAlertApplication.class, args);
+	}
+
+}
---
+++ PortfolioAlert/src/test/java/com/example/PortfolioAlert/PortfolioAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PortfolioAlert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioAlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
